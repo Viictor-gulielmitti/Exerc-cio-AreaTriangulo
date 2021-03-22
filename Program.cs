c41157bfb15514f9f1f3c4198a7c83a2a12fc0e4
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-21 21:36:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TRIANGULO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada1;
+            string entrada2;
+            Area dobro,   Base , Altura;
+
+            Console.WriteLine ( " Digite abaixo como medidas do triangulo, que serão calculadas " );
+            Console.Write ( " Base: " );
+            entrada1  =  Console . ReadLine ();
+            Base =  Convert.ToDouble ( entrada1 );
+            Console.Write ( " Altura: " );
+            entrada2 = Console.ReadLine ();
+            Altura = Convert.ToDouble ( entrada2 );
+            Area  =  Base * Altura / 2 ;
+            Console.WriteLine ( " \n Area " );
+            Console.WriteLine ( $" A Area é: { Area } " );
+            
+    }
+}
